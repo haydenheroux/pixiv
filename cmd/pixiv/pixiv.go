6731b405d253d1df60e67a334f13eecdbefd68f4
Hayden Heroux <haydenheroux@gmail.com>
2022-02-24 19:40:15 -0500

    feat(cli): Displays progress when downloading all illustrations.

--- cmd/pixiv/pixiv.go
+++ cmd/pixiv/pixiv.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"haydenheroux.xyz/pixivapi"
 )
 
 func main() {
 	illustrations, _ := pixivapi.GetTopIllustrations()
-	for _, illust := range illustrations {
-		pixivapi.DownloadIllustration(illust)
+	for n, illust := range illustrations {
+		fmt.Printf("Downloading %d / %d...\n", n, len(illustrations))
+		pixivapi.DownloadIllustration(illust, "test-"+illust.Title+".jpg")
 	}
 }
